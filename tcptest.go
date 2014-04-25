ef0000c7ac7fd39f535b4c6987e0e12d5685af4f
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-25 15:01:06 +0900

    I'm a born again go-hard-tabber

--- tcptest.go
+++ tcptest.go
@@ -1,39 +1,39 @@
 package tcptest
 
 import (
-  "time"
-  "github.com/lestrrat/go-tcputil"
+	"github.com/lestrrat/go-tcputil"
+	"time"
 )
 
 type TCPTest struct {
-  port int
-  exit chan bool
+	port int
+	exit chan bool
 }
 
-func Start(cb func (int), dur time.Duration) (*TCPTest, error) {
-  p, err := tcputil.EmptyPort()
-  if err != nil {
-    return nil, err
-  }
+func Start(cb func(int), dur time.Duration) (*TCPTest, error) {
+	p, err := tcputil.EmptyPort()
+	if err != nil {
+		return nil, err
+	}
 
-  c := make(chan bool, 1)
-  go func(c chan bool, p int) {
-    cb(p)
-    c <-true
-  }(c, p)
+	c := make(chan bool, 1)
+	go func(c chan bool, p int) {
+		cb(p)
+		c <- true
+	}(c, p)
 
-  err = tcputil.WaitLocalPort(p, dur)
-  if err != nil {
-    return nil, err
-  }
+	err = tcputil.WaitLocalPort(p, dur)
+	if err != nil {
+		return nil, err
+	}
 
-  return &TCPTest { p, c }, nil
+	return &TCPTest{p, c}, nil
 }
 
 func (t *TCPTest) Port() int {
-  return t.port
+	return t.port
 }
 
 func (t *TCPTest) Wait() {
-  <-t.exit
+	<-t.exit
 }
--- tcptest_test.go
+++ tcptest_test.go
@@ -1,112 +1,110 @@
 package tcptest
 
 import (
-  "fmt"
-  "log"
-  "net"
-  "os/exec"
-  "syscall"
-  "testing"
-  "time"
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+	"syscall"
+	"testing"
+	"time"
 )
 
 func Example() {
-  var cmd *exec.Cmd
-  memd := func(port int) {
-    cmd = exec.Command("memcached", "-p", fmt.Sprintf("%d", port))
-    cmd.SysProcAttr = &syscall.SysProcAttr {
-      Setpgid: true,
-    }
-    cmd.Run()
-  }
+	var cmd *exec.Cmd
+	memd := func(port int) {
+		cmd = exec.Command("memcached", "-p", fmt.Sprintf("%d", port))
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Setpgid: true,
+		}
+		cmd.Run()
+	}
 
-  server, err := Start(memd, 30 * time.Second)
-  if err != nil {
-    log.Fatalf("Failed to start memcached: %s", err)
-  }
+	server, err := Start(memd, 30*time.Second)
+	if err != nil {
+		log.Fatalf("Failed to start memcached: %s", err)
+	}
 
-  log.Printf("memcached started on port %d", server.Port())
-  defer func() {
-    if cmd != nil && cmd.Process != nil {
-      cmd.Process.Signal(syscall.SIGTERM)
-    }
-  }()
+	log.Printf("memcached started on port %d", server.Port())
+	defer func() {
+		if cmd != nil && cmd.Process != nil {
+			cmd.Process.Signal(syscall.SIGTERM)
+		}
+	}()
 
-  // Do what you want...
+	// Do what you want...
 
+	// When you're done, you need to kill the daemon
+	// because, well, it doesn't unless you tell it to!
+	cmd.Process.Signal(syscall.SIGTERM)
 
-  // When you're done, you need to kill the daemon
-  // because, well, it doesn't unless you tell it to!
-  cmd.Process.Signal(syscall.SIGTERM)
-
-  // Wait for the callback goroutine to exit
-  server.Wait()
+	// Wait for the callback goroutine to exit
+	server.Wait()
 }
 
 func TestBasic(t *testing.T) {
-  cb := func(port int) {
-    l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-    if err != nil {
-      t.Fatalf("Failed to listen on port %d: %s", port, err)
-    }
+	cb := func(port int) {
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		if err != nil {
+			t.Fatalf("Failed to listen on port %d: %s", port, err)
+		}
 
-    // We'll be accessed twice
-    for i := 0; i < 2; i++ {
-      conn, err := l.Accept()
-      if err != nil {
-        t.Fatalf("Failed to accept connection on %d: %s", port, err)
-      }
-      time.Sleep(500 * time.Millisecond)
-      conn.Close()
-    }
-    l.Close()
-  }
+		// We'll be accessed twice
+		for i := 0; i < 2; i++ {
+			conn, err := l.Accept()
+			if err != nil {
+				t.Fatalf("Failed to accept connection on %d: %s", port, err)
+			}
+			time.Sleep(500 * time.Millisecond)
+			conn.Close()
+		}
+		l.Close()
+	}
 
-  t.Logf("Starting callback")
-  server, err := Start(cb, time.Minute)
-  if err != nil {
-    log.Fatalf("Failed to start listening on random port: %s", err)
-  }
+	t.Logf("Starting callback")
+	server, err := Start(cb, time.Minute)
+	if err != nil {
+		log.Fatalf("Failed to start listening on random port: %s", err)
+	}
 
-  t.Logf("Attempting to connect to port %d", server.Port())
-  conn, err := net.Dial("tcp", fmt.Sprintf(":%d", server.Port()))
-  if err != nil {
-    log.Fatalf("Failed to connect to port %d: %s", server.Port(), err)
-  }
-  defer conn.Close()
+	t.Logf("Attempting to connect to port %d", server.Port())
+	conn, err := net.Dial("tcp", fmt.Sprintf(":%d", server.Port()))
+	if err != nil {
+		log.Fatalf("Failed to connect to port %d: %s", server.Port(), err)
+	}
+	defer conn.Close()
 
-  t.Logf("Successfully connected to port %d", server.Port())
+	t.Logf("Successfully connected to port %d", server.Port())
 
-  server.Wait()
+	server.Wait()
 }
 
-
 func TestMemcached(t *testing.T) {
-  // Only run this if we can find a memcached binary in our PATH
-  fqname, err := exec.LookPath("memcached")
-  if err != nil {
-    t.Skip("No memcached available, skipping test")
-  }
+	// Only run this if we can find a memcached binary in our PATH
+	fqname, err := exec.LookPath("memcached")
+	if err != nil {
+		t.Skip("No memcached available, skipping test")
+	}
 
-  t.Logf("Using memcached in %s", fqname)
+	t.Logf("Using memcached in %s", fqname)
 
-  var cmd *exec.Cmd
-  cb := func(port int) {
-    cmd = exec.Command("memcached", "-p", fmt.Sprintf("%d", port))
-    cmd.Run()
-  }
+	var cmd *exec.Cmd
+	cb := func(port int) {
+		cmd = exec.Command("memcached", "-p", fmt.Sprintf("%d", port))
+		cmd.Run()
+	}
 
-  server, err := Start(cb, time.Minute)
-  if err != nil {
-    log.Fatalf("Failed to start listening on random port: %s", err)
-  }
+	server, err := Start(cb, time.Minute)
+	if err != nil {
+		log.Fatalf("Failed to start listening on random port: %s", err)
+	}
 
-  cmd.Process.Signal(syscall.SIGTERM)
+	cmd.Process.Signal(syscall.SIGTERM)
 
-  time.Sleep(5 * time.Second)
+	time.Sleep(5 * time.Second)
 
-  _, err = net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", server.Port()))
-  if err == nil {
-    t.Errorf("After 5 seconds, we can still connect to port %d. Not good", server.Port())
-  }
+	_, err = net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", server.Port()))
+	if err == nil {
+		t.Errorf("After 5 seconds, we can still connect to port %d. Not good", server.Port())
+	}
 }
