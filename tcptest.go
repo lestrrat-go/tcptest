d66b471ce22406728343e187bf33c377a1839c76
Daisuke Maki <lestrrat+github@gmail.com>
2015-04-15 21:14:30 +0900

    Implement Start2(), which gives ways to stop the server

--- tcptest.go
+++ tcptest.go
@@ -1,25 +1,64 @@
 package tcptest
 
 import (
-	"github.com/lestrrat/go-tcputil"
 	"time"
+
+	"github.com/lestrrat/go-tcputil"
 )
 
 type TCPTest struct {
 	port int
-	exit chan bool
+	exit chan struct{}
 }
 
+func (t *TCPTest) start(cb func(*TCPTest)) {
+	defer t.Stop()
+	cb(t)
+}
+
+func (t *TCPTest) Done() <-chan struct{} {
+	return t.exit
+}
+
+// Stop sends a notification through t.Done() that the server should be
+// stopped. It's up to the provider of the callback function to properly
+// exit and clean the server
+func (t *TCPTest) Stop() {
+	t.exit<-struct{}{}
+}
+
+func Start2(cb func(*TCPTest), dur time.Duration) (*TCPTest, error) {
+	p, err := tcputil.EmptyPort()
+	if err != nil {
+		return nil, err
+	}
+
+	t := &TCPTest{
+		port: p,
+		exit: make(chan struct{}, 10), // random buffer number
+	}
+	go t.start(cb)
+
+	if err := tcputil.WaitLocalPort(p, dur); err != nil {
+		close(t.exit)
+		return nil, err
+	}
+
+	return t, nil
+}
+
+// Start is left for backward compatibility, but you should use
+// Start2(), which allows better management of the lifecycle
 func Start(cb func(int), dur time.Duration) (*TCPTest, error) {
 	p, err := tcputil.EmptyPort()
 	if err != nil {
 		return nil, err
 	}
 
-	c := make(chan bool, 1)
-	go func(c chan bool, p int) {
+	c := make(chan struct{}, 10)
+	go func(c chan struct{}, p int) {
+		defer func() { c <- struct{}{} }()
 		cb(p)
-		c <- true
 	}(c, p)
 
 	err = tcputil.WaitLocalPort(p, dur)
--- tcptest_test.go
+++ tcptest_test.go
@@ -12,15 +12,23 @@ import (
 
 func Example() {
 	var cmd *exec.Cmd
-	memd := func(port int) {
-		cmd = exec.Command("memcached", "-p", fmt.Sprintf("%d", port))
+	memd := func(t *TCPTest) {
+		cmd = exec.Command("memcached", "-p", fmt.Sprintf("%d", t.Port()))
 		cmd.SysProcAttr = &syscall.SysProcAttr{
 			Setpgid: true,
 		}
-		cmd.Run()
+
+		go cmd.Run()
+		for loop := true; loop; {
+			select {
+			case <-t.Done():
+				cmd.Process.Kill()
+				loop = false
+			}
+		}
 	}
 
-	server, err := Start(memd, 30*time.Second)
+	server, err := Start2(memd, 30*time.Second)
 	if err != nil {
 		log.Fatalf("Failed to start memcached: %s", err)
 	}
@@ -36,7 +44,7 @@ func Example() {
 
 	// When you're done, you need to kill the daemon
 	// because, well, it doesn't unless you tell it to!
-	cmd.Process.Signal(syscall.SIGTERM)
+	server.Stop()
 
 	// Wait for the callback goroutine to exit
 	server.Wait()
@@ -79,6 +87,43 @@ func TestBasic(t *testing.T) {
 	server.Wait()
 }
 
+func TestBasic2(t *testing.T) {
+	cb := func(tt *TCPTest) {
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", tt.Port()))
+		if err != nil {
+			t.Fatalf("Failed to listen on port %d: %s", tt.Port(), err)
+		}
+
+		// We'll be accessed twice
+		for i := 0; i < 2; i++ {
+			conn, err := l.Accept()
+			if err != nil {
+				t.Fatalf("Failed to accept connection on %d: %s", tt.Port(), err)
+			}
+			time.Sleep(500 * time.Millisecond)
+			conn.Close()
+		}
+		l.Close()
+	}
+
+	t.Logf("Starting callback")
+	server, err := Start2(cb, time.Minute)
+	if err != nil {
+		log.Fatalf("Failed to start listening on random port: %s", err)
+	}
+
+	t.Logf("Attempting to connect to port %d", server.Port())
+	conn, err := net.Dial("tcp", fmt.Sprintf(":%d", server.Port()))
+	if err != nil {
+		log.Fatalf("Failed to connect to port %d: %s", server.Port(), err)
+	}
+	defer conn.Close()
+
+	t.Logf("Successfully connected to port %d", server.Port())
+
+	server.Wait()
+}
+
 func TestMemcached(t *testing.T) {
 	// Only run this if we can find a memcached binary in our PATH
 	fqname, err := exec.LookPath("memcached")
@@ -99,7 +144,43 @@ func TestMemcached(t *testing.T) {
 		log.Fatalf("Failed to start listening on random port: %s", err)
 	}
 
-	cmd.Process.Signal(syscall.SIGTERM)
+	cmd.Process.Kill()
+
+	time.Sleep(5 * time.Second)
+
+	_, err = net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", server.Port()))
+	if err == nil {
+		t.Errorf("After 5 seconds, we can still connect to port %d. Not good", server.Port())
+	}
+}
+func TestMemcached2(t *testing.T) {
+	// Only run this if we can find a memcached binary in our PATH
+	fqname, err := exec.LookPath("memcached")
+	if err != nil {
+		t.Skip("No memcached available, skipping test")
+	}
+
+	t.Logf("Using memcached in %s", fqname)
+
+	var cmd *exec.Cmd
+	cb := func(tt *TCPTest) {
+		cmd = exec.Command("memcached", "-p", fmt.Sprintf("%d", tt.Port()))
+		go cmd.Run()
+		for loop := true; loop; {
+			select {
+			case <-tt.Done():
+				cmd.Process.Kill()
+				loop = false
+			}
+		}
+	}
+
+	server, err := Start2(cb, time.Minute)
+	if err != nil {
+		log.Fatalf("Failed to start listening on random port: %s", err)
+	}
+
+	server.Stop()
 
 	time.Sleep(5 * time.Second)
 
